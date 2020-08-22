e67ec8722fb9233722d6d4e29275a3dbd8323cb2
Siddartha1997 <siddartha.suthraye97@gmail.com>
2020-08-22 15:21:32 +0000

    first commit2

---
+++ build/precache-manifest.31be3f1bd465c65673da67d9750c0ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a3f498b291d9e18f030c2800ceeac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8034537dc4aa81cb42",
+    "url": "/static/css/main.e01144e3.chunk.css"
+  },
+  {
+    "revision": "d1fa88da5117d98ef3ed",
+    "url": "/static/js/2.3a8401e2.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.3a8401e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8034537dc4aa81cb42",
+    "url": "/static/js/main.19bbe254.chunk.js"
+  },
+  {
+    "revision": "36c812ea3fc5bdf3cc92",
+    "url": "/static/js/runtime-main.9524a792.js"
+  }
+]);
